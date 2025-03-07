0a47913d54ead7ba7464803fd12345de63a3d63f
jhalejandrohu <104876718+jhalejandrohu@users.noreply.github.com>
2025-03-07 17:55:37 -0500

    crear lista de amigos
    Se crea la función agregarAmigo para guardad los nombres en una lista.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombreAmigos = [];
+
+
+function agregarAmigo(){
+
+    let nombreDeAmigo = document.getElementById('amigo').value;
+    console.log(nombreDeAmigo);
+    if (nombreDeAmigo) { 
+        listaNombreAmigos.push(nombreDeAmigo);
+        console.log(listaNombreAmigos);
+        return nombreDeAmigo;
+        
+    } else {
+        console.log(listaNombreAmigos);
+        return null;
+    }
+   
+}
