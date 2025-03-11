83e258e9a9785ac066ae09d6e5d45ac46cdfc2ce
jhalejandrohu <104876718+jhalejandrohu@users.noreply.github.com>
2025-03-11 15:30:41 -0500

    version final del proyecto
    Se agregan las funciones vaciarLista y actualizarLista para que el proyecto funcione de forma correcta

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaNombreAmigos = [];
+// Funcion encargada de vaciar la lista una vez se haya realizado el sorteo
+function vaciarLista(){
 
+    listaNombreAmigos = [];
 
+}
+// funcion encargada de actualizar los nombres a mostrar en pantalla
+function actualizarLista() {
+    // Limpiar la lista antes de agregar nuevos elementos
+    resultado.innerHTML = "";
+    // Recorrer el array y agregar cada amigo como un <li>
+    for (let amigo of listaNombreAmigos) {       
+        resultado.innerHTML += `<li>${amigo}</li>`;
+    }
+}
+// Funcion encargada de agregar nuevos amigos
 function agregarAmigo(){
 
     let nombreDeAmigo = document.getElementById('amigo').value;
@@ -10,6 +24,7 @@ function agregarAmigo(){
         listaNombreAmigos.push(nombreDeAmigo);
         console.log(listaNombreAmigos);
         document.getElementById("amigo").value = "";
+        actualizarLista();
         return nombreDeAmigo;
         
     } else {
@@ -18,13 +33,16 @@ function agregarAmigo(){
     }
    
 }
-
+// funcion encargada de sortear los amigos
 function sortearAmigo(){
-    
+    let amigoSorteado 
     if(listaNombreAmigos.length === 0){
         return alert("¡no existen nombres para sortear!");
     } else{
         let numeroGenerado =  Math.floor(Math.random()*listaNombreAmigos.length);
-        return alert(listaNombreAmigos[numeroGenerado]);
+        amigoSorteado = listaNombreAmigos[numeroGenerado]
+        // Mostrar el resultado en el HTML
+        resultado.innerHTML = `El amigo seleccionado es: <strong>${amigoSorteado}</strong>`;
+        vaciarLista();
     }
 }
